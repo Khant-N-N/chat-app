43cbb29f08181dc05e955b2759aa4f9be37bedce
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2024-07-07 22:16:37 +0630

    cors origin fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,8 +10,8 @@ import cors from "cors";
 import { authUser } from "./utils/authUser";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("hello!");
