ee73024b25c7921060eaead5d27c00ca4af4f771
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2024-07-09 23:27:29 +0630

    REgiter user complete

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,14 +1,56 @@
+import { isAxiosError } from "axios";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { baseUrl, postReq } from "../services/api";
+import { useDispatch } from "react-redux";
+import { setAuthenticatedUser } from "../features/userSlice";
 
 const Register = () => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [FormData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFormData({ ...FormData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const url = baseUrl + "/register";
+      const body = { ...FormData };
+      const data = await postReq({ url, body });
+      dispatch(setAuthenticatedUser(data));
+      setLoading(false);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      if (isAxiosError(error)) {
+        setErrorMessage(error.response?.data?.error);
+      } else {
+        setErrorMessage("Something went wrong, please try again.");
+      }
+      console.error(error);
+    }
+  };
   return (
-    <div className="flex justify-center items-center h-screen flex-col bg-primary text-white gap-3">
+    <form
+      onSubmit={handleSubmit}
+      className="flex justify-center items-center h-screen flex-col bg-primary text-white gap-3"
+    >
       <p className="text-secondary text-[21px]">Create an Account</p>
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
 
       <div>
         <p>Email :</p>
         <input
+          value={FormData.email}
+          onChange={handleChange}
           className="border-none text-primary focus:outline-secondary rounded-xl p-3 w-[20rem]"
           type="email"
           name="email"
@@ -20,6 +62,8 @@ const Register = () => {
       <div>
         <p>Username :</p>
         <input
+          value={FormData.username}
+          onChange={handleChange}
           className="border-none text-primary focus:outline-secondary rounded-xl p-3 w-[20rem]"
           type="text"
           name="username"
@@ -31,6 +75,8 @@ const Register = () => {
       <div>
         <p>Password :</p>
         <input
+          value={FormData.password}
+          onChange={handleChange}
           className="border-none text-primary focus:outline-secondary rounded-xl p-3 w-[20rem]"
           type="password"
           name="password"
@@ -39,7 +85,8 @@ const Register = () => {
         />
       </div>
       <button
-        type="button"
+        disabled={loading}
+        type="submit"
         className="border-none bg-secondary focus:outline-secondary rounded-xl py-3 my-4 w-[20rem] hover:opacity-65"
       >
         Register
@@ -53,7 +100,7 @@ const Register = () => {
           Log In
         </Link>
       </p>
-    </div>
+    </form>
   );
 };
 
