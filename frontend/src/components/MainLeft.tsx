8a8158f41f8efc61287d0e60fce6c8fd4d41784b
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2024-10-03 23:16:00 +0630

    fixed chat _id

--- frontend/src/components/MainLeft.tsx
+++ frontend/src/components/MainLeft.tsx
@@ -2,23 +2,29 @@ import { useEffect, useState } from "react";
 import ChatUser from "./ChatUser";
 import { baseUrl, getReq } from "../services/api";
 import { ChatModel } from "../models/chatModel";
+import SearchBox from "./SearchBox";
 
 const MainLeft = () => {
   const [myChats, setMyChats] = useState<ChatModel[] | null>(null);
   useEffect(() => {
     const getChats = async () => {
       const chats = (await getReq(`${baseUrl}/chat`)) as ChatModel[];
-      setMyChats(chats);
+      chats.length ? setMyChats(chats) : setMyChats(null);
     };
     getChats();
   }, []);
   return (
-    <div className="w-1/2 border rounded-s">
-      {myChats
-        ? myChats.map((chat) => {
-            return <ChatUser chat={chat} key={chat.id} />;
-          })
-        : "Add new friends to start messaging"}
+    <div className="w-full md:w-1/2 border rounded-s py-3 flex flex-col items-center gap-2">
+      <SearchBox />
+      {myChats ? (
+        myChats.map((chat) => {
+          return <ChatUser chat={chat} key={chat._id} />;
+        })
+      ) : (
+        <div className="h-full flex items-center">
+          Add new friends to start messaging
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/models/chatModel.ts
+++ frontend/src/models/chatModel.ts
@@ -1,5 +1,5 @@
 export interface ChatModel {
-  id: string;
+  _id: string;
   chatname: string;
   createdAt: string;
   updatedAt: string;
